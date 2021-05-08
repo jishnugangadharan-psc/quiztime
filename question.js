63144d92ccf9ba4b5620ea8f419c1993897358f0
vishgs <vishnugs2016@gmail.com>
2021-05-08 19:20:47 +0530

    added files

---
+++ question.js
@@ -0,0 +1,161 @@
+// question 1 and its options
+var question1 = "1. what is the unit of energy ?"
+var q1o1 = "joule"
+var q1o2 = "newton"
+var q1o3 = "decibel"
+var q1o4 = "candela"
+var answer1 = "joule"
+
+// question 2 and its options
+var question2 = "question 2"
+var q2o1 = "optionA"
+var q2o2 = "optionB"
+var q2o3 = "optionC"
+var q2o4 = "optionD"
+var answer2 = "optionC"
+
+// question 3 and its options
+var question3 = "question 3"
+var q3o1 = "optionA"
+var q3o2 = "optionB"
+var q3o3 = "optionC"
+var q3o4 = "optionD"
+var answer3 = "optionC"
+
+// question 4 and its options
+var question4 = "question 4"
+var q4o1 = "optionA"
+var q4o2 = "optionB"
+var q4o3 = "optionC"
+var q4o4 = "optionD"
+var answer4 = "optionC"
+
+// question 5 and its options
+var question5 = "question 5"
+var q5o1 = "optionA"
+var q5o2 = "optionB"
+var q5o3 = "optionC"
+var q5o4 = "optionD"
+var answer5 = "optionC"
+
+// question 6 and its options
+var question6 = "question 6"
+var q6o1 = "optionA"
+var q6o2 = "optionB"
+var q6o3 = "optionC"
+var q6o4 = "optionD"
+var answer6 = "optionC"
+
+// question 7 and its options
+var question7 = "question 7"
+var q7o1 = "optionA"
+var q7o2 = "optionB"
+var q7o3 = "optionC"
+var q7o4 = "optionD"
+var answer7 = "optionC"
+
+// question 8 and its options
+var question8 = "question 8"
+var q8o1 = "optionA"
+var q8o2 = "optionB"
+var q8o3 = "optionC"
+var q8o4 = "optionD"
+var answer8 = "optionC"
+
+// question 9 and its options
+var question9 = "question 9"
+var q9o1 = "optionA"
+var q9o2 = "optionB"
+var q9o3 = "optionC"
+var q9o4 = "optionD"
+var answer9 = "optionC"
+
+// question 10 and its options
+var question10 = "question 10"
+var q10o1 = "optionA"
+var q10o2 = "optionB"
+var q10o3 = "optionC"
+var q10o4 = "optionD"
+var answer10 = "optionC"
+
+// question 11 and its options
+var question11 = "question 11"
+var q11o1 = "optionA"
+var q11o2 = "optionB"
+var q11o3 = "optionC"
+var q11o4 = "optionD"
+var answer11 = "optionC"
+
+// question 12 and its options
+var question12 = "question 12"
+var q12o1 = "optionA"
+var q12o2 = "optionB"
+var q12o3 = "optionC"
+var q12o4 = "optionD"
+var answer12 = "optionC"
+
+// question 13 and its options
+var question13 = "question 13"
+var q13o1 = "optionA"
+var q13o2 = "optionB"
+var q13o3 = "optionC"
+var q13o4 = "optionD"
+var answer13 = "optionC"
+
+// question 14 and its options
+var question14 = "question 14"
+var q14o1 = "optionA"
+var q14o2 = "optionB"
+var q14o3 = "optionC"
+var q14o4 = "optionD"
+var answer14 = "optionC"
+
+// question 15 and its options
+var question15 = "question 15"
+var q15o1 = "optionA"
+var q15o2 = "optionB"
+var q15o3 = "optionC"
+var q15o4 = "optionD"
+var answer15 = "optionC"
+
+// question 16 and its options
+var question16 = "question 16"
+var q16o1 = "optionA"
+var q16o2 = "optionB"
+var q16o3 = "optionC"
+var q16o4 = "optionD"
+var answer16 = "optionC"
+
+// question 17 and its options
+var question17 = "question 17"
+var q17o1 = "optionA"
+var q17o2 = "optionB"
+var q17o3 = "optionC"
+var q17o4 = "optionD"
+var answer17 = "optionC"
+
+// question 18 and its options
+var question18 = "question 18"
+var q18o1 = "optionA"
+var q18o2 = "optionB"
+var q18o3 = "optionC"
+var q18o4 = "optionD"
+var answer18 = "optionC"
+
+// question 19 and its options
+var question19 = "question 19"
+var q19o1 = "optionA"
+var q19o2 = "optionB"
+var q19o3 = "optionC"
+var q19o4 = "optionD"
+var answer19 = "optionC"
+
+// question 20 and its options
+var question20 = "question 20"
+var q20o1 = "optionA"
+var q20o2 = "optionB"
+var q20o3 = "optionC"
+var q20o4 = "optionD"
+var answer20 = "optionC"
+
+
---
+++ score.js
@@ -0,0 +1,417 @@
+var correct = []
+var wrong = []
+function myFunction() {
+
+    var ele1 = document.getElementsByName('q1o');
+    var ele2 = document.getElementsByName('q2o');
+    var ele3 = document.getElementsByName('q3o');
+    var ele4 = document.getElementsByName('q4o');
+    var ele5 = document.getElementsByName('q5o');
+    var ele6 = document.getElementsByName('q6o');
+    var ele7 = document.getElementsByName('q7o');
+    var ele8 = document.getElementsByName('q8o');
+    var ele9 = document.getElementsByName('q9o');
+    var ele10 = document.getElementsByName('q10o');
+    var ele11 = document.getElementsByName('q11o');
+    var ele12 = document.getElementsByName('q12o');
+    var ele13 = document.getElementsByName('q13o');
+    var ele14 = document.getElementsByName('q14o');
+    var ele15 = document.getElementsByName('q15o');
+    var ele16 = document.getElementsByName('q16o');
+    var ele17 = document.getElementsByName('q17o');
+    var ele18 = document.getElementsByName('q18o');
+    var ele19 = document.getElementsByName('q19o');
+    var ele20 = document.getElementsByName('q20o');
+
+    // 1
+    for (i = 0; i < ele1.length; i++) {
+        if (ele1[i].checked) {
+
+            if (ele1[i].value == answer1) {
+                // window.alert("sheri")
+                correct.push(1)
+            }
+            else {
+
+                wrong.push(1)
+            }
+
+        }
+    }
+
+    // 2
+    for (i = 0; i < ele2.length; i++) {
+        if (ele2[i].checked) {
+
+            if (ele2[i].value == answer2) {
+                // window.alert("sheri")
+                correct.push(1)
+
+            }
+            else {
+
+                wrong.push(1)
+            }
+
+        }
+    }
+
+    // 3
+    for (i = 0; i < ele3.length; i++) {
+        if (ele3[i].checked) {
+
+            if (ele3[i].value == answer3) {
+                // window.alert("sheri")
+                correct.push(1)
+
+            }
+            else {
+
+                wrong.push(1)
+            }
+
+        }
+    }
+
+    // 4
+    for (i = 0; i < ele4.length; i++) {
+        if (ele4[i].checked) {
+
+            if (ele4[i].value == answer4) {
+                // window.alert("sheri")
+                correct.push(1)
+
+            }
+            else {
+
+                wrong.push(1)
+            }
+
+        }
+    }
+
+    // 5
+    for (i = 0; i < ele5.length; i++) {
+        if (ele5[i].checked) {
+
+            if (ele5[i].value == answer5) {
+                // window.alert("sheri")
+                correct.push(1)
+
+            }
+            else {
+
+                wrong.push(1)
+            }
+
+        }
+    }
+
+    // 6
+    for (i = 0; i < ele6.length; i++) {
+        if (ele6[i].checked) {
+
+            if (ele6[i].value == answer6) {
+                // window.alert("sheri")
+                correct.push(1)
+
+            }
+            else {
+
+                wrong.push(1)
+               
+            }
+
+        }
+    }
+
+    // 7
+    for (i = 0; i < ele7.length; i++) {
+        if (ele7[i].checked) {
+
+            if (ele7[i].value == answer7) {
+                // window.alert("sheri")
+                correct.push(1)
+                
+
+            }
+            else {
+
+                wrong.push(1)
+                
+            }
+
+        }
+    }
+
+    // 8
+    for (i = 0; i < ele8.length; i++) {
+        if (ele8[i].checked) {
+
+            if (ele8[i].value == answer8) {
+                // window.alert("sheri")
+                correct.push(1)
+
+            }
+            else {
+
+                wrong.push(1)
+            }
+
+        }
+    }
+
+    // 9
+    for (i = 0; i < ele9.length; i++) {
+        if (ele9[i].checked) {
+
+            if (ele9[i].value == answer9) {
+                // window.alert("sheri")
+                correct.push(1)
+
+            }
+            else {
+
+                wrong.push(1)
+            }
+
+        }
+    }
+
+    // 10
+    for (i = 0; i < ele10.length; i++) {
+        if (ele10[i].checked) {
+
+            if (ele10[i].value == answer10) {
+                // window.alert("sheri")
+                correct.push(1)
+
+            }
+            else {
+
+                wrong.push(1)
+            }
+
+        }
+    }
+
+    // 11
+    for (i = 0; i < ele11.length; i++) {
+        if (ele11[i].checked) {
+
+            if (ele11[i].value == answer11) {
+                // window.alert("sheri")
+                correct.push(1)
+
+            }
+            else {
+
+                wrong.push(1)
+            }
+
+        }
+    }
+
+    // 12
+    for (i = 0; i < ele12.length; i++) {
+        if (ele12[i].checked) {
+
+            if (ele12[i].value == answer12) {
+                // window.alert("sheri")
+                correct.push(1)
+
+            }
+            else {
+
+                wrong.push(1)
+            }
+
+        }
+    }
+
+    // 13
+    for (i = 0; i < ele13.length; i++) {
+        if (ele13[i].checked) {
+
+            if (ele13[i].value == answer13) {
+                // window.alert("sheri")
+                correct.push(1)
+
+            }
+            else {
+
+                wrong.push(1)
+            }
+
+        }
+    }
+
+    // 14
+    for (i = 0; i < ele14.length; i++) {
+        if (ele14[i].checked) {
+
+            if (ele14[i].value == answer14) {
+                // window.alert("sheri")
+                correct.push(1)
+
+            }
+            else {
+
+                wrong.push(1)
+            }
+
+        }
+    }
+
+    // 15
+    for (i = 0; i < ele15.length; i++) {
+        if (ele15[i].checked) {
+
+            if (ele15[i].value == answer15) {
+                // window.alert("sheri")
+                correct.push(1)
+
+            }
+            else {
+
+                wrong.push(1)
+            }
+
+        }
+    }
+
+    // 16
+    for (i = 0; i < ele16.length; i++) {
+        if (ele16[i].checked) {
+
+            if (ele16[i].value == answer16) {
+                // window.alert("sheri")
+                correct.push(1)
+
+            }
+            else {
+
+                wrong.push(1)
+            }
+
+        }
+    }
+
+    // 17
+    for (i = 0; i < ele17.length; i++) {
+        if (ele17[i].checked) {
+
+            if (ele17[i].value == answer17) {
+                // window.alert("sheri")
+                correct.push(1)
+
+            }
+            else {
+
+                wrong.push(1)
+            }
+
+        }
+    }
+
+    // 18
+    for (i = 0; i < ele18.length; i++) {
+        if (ele18[i].checked) {
+
+            if (ele18[i].value == answer18) {
+                // window.alert("sheri")
+                correct.push(1)
+
+            }
+            else {
+
+                wrong.push(1)
+            }
+
+        }
+    }
+
+    // 19
+    for (i = 0; i < ele19.length; i++) {
+        if (ele19[i].checked) {
+
+            if (ele19[i].value == answer19) {
+                // window.alert("sheri")
+                correct.push(1)
+
+            }
+            else {
+
+                wrong.push(1)
+            }
+
+        }
+    }
+
+    // 20
+    for (i = 0; i < ele20.length; i++) {
+        if (ele20[i].checked) {
+
+            if (ele20[i].value == answer20) {
+                // window.alert("sheri")
+                correct.push(1)
+
+            }
+            else {
+
+                wrong.push(1)
+            }
+
+        }
+    }
+
+    // total correct
+    var totalCorrect = 0;
+    for (var i = 0; i < correct.length; i++) {
+        totalCorrect += correct[i];
+    }
+
+    // total incorrect
+    var totalWrong = 0;
+    var wrongAnswers = 0;
+    for (var i = 0; i < wrong.length; i++) {
+        wrongAnswers += wrong[i];
+        totalWrong = (0.3333*wrongAnswers)
+    }
+
+    var total = totalCorrect - totalWrong
+    var questionsAttended = totalCorrect+wrongAnswers;
+    document.getElementById("questions_attended").innerHTML = questionsAttended
+    document.getElementById("correct").innerHTML = totalCorrect
+    document.getElementById("wrong").innerHTML = wrongAnswers
+    document.getElementById("total_marks").innerHTML = total
+
+    document.getElementById("questionDiv").style.display = "none"
+    document.getElementById("result_section").style.display = "block"
+    document.getElementById("submit").style.display = "none"
+
+}
+
+function ckChange(ckType){
+
+    var ckName = document.getElementsByName(ckType.name);
+    var checked = document.getElementById(ckType.id);
+
+    if (checked.checked) {
+      for(var i=0; i < ckName.length; i++){
+
+          if(!ckName[i].checked){
+              ckName[i].disabled = true;
+          }else{
+              ckName[i].disabled = false;
+          }
+      } 
+    }
+    else {
+      for(var i=0; i < ckName.length; i++){
+        ckName[i].disabled = false;
+      } 
+    }
+    
+    
+}
