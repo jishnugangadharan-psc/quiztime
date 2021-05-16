e5765757c9433ced55bd7ab11a1e2248158446b5
vishgs <vishnugs2016@gmail.com>
2021-05-16 19:26:12 +0530

    added new version

--- score.js
+++ score.js
@@ -1,367 +1,27 @@
 var correct = []
 var wrong = []
+var answers = []
 function myFunction() {
 
-    var ele1 = document.getElementsByName('q1o');
-    var ele2 = document.getElementsByName('q2o');
-    var ele3 = document.getElementsByName('q3o');
-    var ele4 = document.getElementsByName('q4o');
-    var ele5 = document.getElementsByName('q5o');
-    var ele6 = document.getElementsByName('q6o');
-    var ele7 = document.getElementsByName('q7o');
-    var ele8 = document.getElementsByName('q8o');
-    var ele9 = document.getElementsByName('q9o');
-    var ele10 = document.getElementsByName('q10o');
-    var ele11 = document.getElementsByName('q11o');
-    var ele12 = document.getElementsByName('q12o');
-    var ele13 = document.getElementsByName('q13o');
-    var ele14 = document.getElementsByName('q14o');
-    var ele15 = document.getElementsByName('q15o');
-    var ele16 = document.getElementsByName('q16o');
-    var ele17 = document.getElementsByName('q17o');
-    var ele18 = document.getElementsByName('q18o');
-    var ele19 = document.getElementsByName('q19o');
-    var ele20 = document.getElementsByName('q20o');
+    for (i = 1; i <= 20; i++) {
 
-    // 1
-    for (i = 0; i < ele1.length; i++) {
-        if (ele1[i].checked) {
+        var element = document.getElementsByName('q' + i + 'o');
 
-            if (ele1[i].value == answer1) {
-                // window.alert("sheri")
-                correct.push(1)
+        for (j = 0; j < element.length; j++) {
+
+            if (element[j].checked) {
+
+                if (element[j].value == correctAnswers[(i - 1)]) {
+                    // window.alert("sheri")
+                    correct.push(1)
+                    answers.push(i + ". " + element[j].value)
+                }
+                else {
+
+                    wrong.push(1)
+                    answers.push(i + ". " + element[j].value)
+                }
             }
-            else {
-
-                wrong.push(1)
-            }
-
-        }
-    }
-
-    // 2
-    for (i = 0; i < ele2.length; i++) {
-        if (ele2[i].checked) {
-
-            if (ele2[i].value == answer2) {
-                // window.alert("sheri")
-                correct.push(1)
-
-            }
-            else {
-
-                wrong.push(1)
-            }
-
-        }
-    }
-
-    // 3
-    for (i = 0; i < ele3.length; i++) {
-        if (ele3[i].checked) {
-
-            if (ele3[i].value == answer3) {
-                // window.alert("sheri")
-                correct.push(1)
-
-            }
-            else {
-
-                wrong.push(1)
-            }
-
-        }
-    }
-
-    // 4
-    for (i = 0; i < ele4.length; i++) {
-        if (ele4[i].checked) {
-
-            if (ele4[i].value == answer4) {
-                // window.alert("sheri")
-                correct.push(1)
-
-            }
-            else {
-
-                wrong.push(1)
-            }
-
-        }
-    }
-
-    // 5
-    for (i = 0; i < ele5.length; i++) {
-        if (ele5[i].checked) {
-
-            if (ele5[i].value == answer5) {
-                // window.alert("sheri")
-                correct.push(1)
-
-            }
-            else {
-
-                wrong.push(1)
-            }
-
-        }
-    }
-
-    // 6
-    for (i = 0; i < ele6.length; i++) {
-        if (ele6[i].checked) {
-
-            if (ele6[i].value == answer6) {
-                // window.alert("sheri")
-                correct.push(1)
-
-            }
-            else {
-
-                wrong.push(1)
-               
-            }
-
-        }
-    }
-
-    // 7
-    for (i = 0; i < ele7.length; i++) {
-        if (ele7[i].checked) {
-
-            if (ele7[i].value == answer7) {
-                // window.alert("sheri")
-                correct.push(1)
-                
-
-            }
-            else {
-
-                wrong.push(1)
-                
-            }
-
-        }
-    }
-
-    // 8
-    for (i = 0; i < ele8.length; i++) {
-        if (ele8[i].checked) {
-
-            if (ele8[i].value == answer8) {
-                // window.alert("sheri")
-                correct.push(1)
-
-            }
-            else {
-
-                wrong.push(1)
-            }
-
-        }
-    }
-
-    // 9
-    for (i = 0; i < ele9.length; i++) {
-        if (ele9[i].checked) {
-
-            if (ele9[i].value == answer9) {
-                // window.alert("sheri")
-                correct.push(1)
-
-            }
-            else {
-
-                wrong.push(1)
-            }
-
-        }
-    }
-
-    // 10
-    for (i = 0; i < ele10.length; i++) {
-        if (ele10[i].checked) {
-
-            if (ele10[i].value == answer10) {
-                // window.alert("sheri")
-                correct.push(1)
-
-            }
-            else {
-
-                wrong.push(1)
-            }
-
-        }
-    }
-
-    // 11
-    for (i = 0; i < ele11.length; i++) {
-        if (ele11[i].checked) {
-
-            if (ele11[i].value == answer11) {
-                // window.alert("sheri")
-                correct.push(1)
-
-            }
-            else {
-
-                wrong.push(1)
-            }
-
-        }
-    }
-
-    // 12
-    for (i = 0; i < ele12.length; i++) {
-        if (ele12[i].checked) {
-
-            if (ele12[i].value == answer12) {
-                // window.alert("sheri")
-                correct.push(1)
-
-            }
-            else {
-
-                wrong.push(1)
-            }
-
-        }
-    }
-
-    // 13
-    for (i = 0; i < ele13.length; i++) {
-        if (ele13[i].checked) {
-
-            if (ele13[i].value == answer13) {
-                // window.alert("sheri")
-                correct.push(1)
-
-            }
-            else {
-
-                wrong.push(1)
-            }
-
-        }
-    }
-
-    // 14
-    for (i = 0; i < ele14.length; i++) {
-        if (ele14[i].checked) {
-
-            if (ele14[i].value == answer14) {
-                // window.alert("sheri")
-                correct.push(1)
-
-            }
-            else {
-
-                wrong.push(1)
-            }
-
-        }
-    }
-
-    // 15
-    for (i = 0; i < ele15.length; i++) {
-        if (ele15[i].checked) {
-
-            if (ele15[i].value == answer15) {
-                // window.alert("sheri")
-                correct.push(1)
-
-            }
-            else {
-
-                wrong.push(1)
-            }
-
-        }
-    }
-
-    // 16
-    for (i = 0; i < ele16.length; i++) {
-        if (ele16[i].checked) {
-
-            if (ele16[i].value == answer16) {
-                // window.alert("sheri")
-                correct.push(1)
-
-            }
-            else {
-
-                wrong.push(1)
-            }
-
-        }
-    }
-
-    // 17
-    for (i = 0; i < ele17.length; i++) {
-        if (ele17[i].checked) {
-
-            if (ele17[i].value == answer17) {
-                // window.alert("sheri")
-                correct.push(1)
-
-            }
-            else {
-
-                wrong.push(1)
-            }
-
-        }
-    }
-
-    // 18
-    for (i = 0; i < ele18.length; i++) {
-        if (ele18[i].checked) {
-
-            if (ele18[i].value == answer18) {
-                // window.alert("sheri")
-                correct.push(1)
-
-            }
-            else {
-
-                wrong.push(1)
-            }
-
-        }
-    }
-
-    // 19
-    for (i = 0; i < ele19.length; i++) {
-        if (ele19[i].checked) {
-
-            if (ele19[i].value == answer19) {
-                // window.alert("sheri")
-                correct.push(1)
-
-            }
-            else {
-
-                wrong.push(1)
-            }
-
-        }
-    }
-
-    // 20
-    for (i = 0; i < ele20.length; i++) {
-        if (ele20[i].checked) {
-
-            if (ele20[i].value == answer20) {
-                // window.alert("sheri")
-                correct.push(1)
-
-            }
-            else {
-
-                wrong.push(1)
-            }
-
         }
     }
 
@@ -376,11 +36,11 @@ function myFunction() {
     var wrongAnswers = 0;
     for (var i = 0; i < wrong.length; i++) {
         wrongAnswers += wrong[i];
-        totalWrong = (0.3333*wrongAnswers)
+        totalWrong = (0.3333 * wrongAnswers)
     }
 
     var total = totalCorrect - totalWrong
-    var questionsAttended = totalCorrect+wrongAnswers;
+    var questionsAttended = totalCorrect + wrongAnswers;
     document.getElementById("questions_attended").innerHTML = questionsAttended
     document.getElementById("correct").innerHTML = totalCorrect
     document.getElementById("wrong").innerHTML = wrongAnswers
@@ -390,28 +50,34 @@ function myFunction() {
     document.getElementById("result_section").style.display = "block"
     document.getElementById("submit").style.display = "none"
 
+    result()
+
 }
 
-function ckChange(ckType){
+function ckChange(ckType) {
 
     var ckName = document.getElementsByName(ckType.name);
     var checked = document.getElementById(ckType.id);
 
     if (checked.checked) {
-      for(var i=0; i < ckName.length; i++){
+        for (var i = 0; i < ckName.length; i++) {
 
-          if(!ckName[i].checked){
-              ckName[i].disabled = true;
-          }else{
-              ckName[i].disabled = false;
-          }
-      } 
+            if (!ckName[i].checked) {
+                ckName[i].disabled = true;
+            } else {
+                ckName[i].disabled = false;
+            }
+        }
     }
     else {
-      for(var i=0; i < ckName.length; i++){
-        ckName[i].disabled = false;
-      } 
+        for (var i = 0; i < ckName.length; i++) {
+            ckName[i].disabled = false;
+        }
     }
-    
-    
+}
+
+function result() {
+
+    document.getElementById("sAnswers").innerHTML = answers.splice(',').join('<br>')
+
 }
